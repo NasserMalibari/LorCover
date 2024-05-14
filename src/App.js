62aef197b56297ef91bc4a5639f1ace3aa6cacc8
Nasser Malibari <nassermalibari1@gmail.com>
2024-05-14 13:16:46 +1000

    simple browser router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Page1 from './pages/page1';
+import Page2 from './pages/page2';
 
 function App() {
   const [currTime, setCurrTime] = useState(0);
@@ -14,24 +17,35 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi Holly.
-          {/* Edit <code>src/App.js</code> and SAVE to RELOAD. */}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currTime}</p>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Page1 />}></Route>
+        <Route path="/page1" element={<Page1 />}></Route>
+        <Route path="/page2" element={<Page2 />}></Route>
+      </Routes>
+    
+    </BrowserRouter>
+    </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <Page1 />
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Hi Holly.
+    //       {/* Edit <code>src/App.js</code> and SAVE to RELOAD. */}
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <p>The current time is {currTime}</p>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/pages/page2.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Page2() {
+  return <>
+    Page2
+  </>
+}
+
+export default Page2;
