bf2c223bfd184a46243b185e66bf3dfec61d3f63
Nasser Malibari <nassermalibari1@gmail.com>
2024-05-14 13:24:22 +1000

    simple 2 page spa

--- src/pages/page1.js
+++ src/pages/page1.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function Page1() {
+
+  const navigate = useNavigate();
   return <>
     Page1
-  </>
+    <Button variant="contained" onClick={() => navigate('/page2')}>Page2</Button>
+    </>
 }
 
 export default Page1;
--- src/pages/page2.js
+++ src/pages/page2.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function Page2() {
+  const navigate = useNavigate();
+
   return <>
     Page2
+    <Button variant="contained" onClick={() => navigate('/page1')}>Page1</Button>
   </>
 }
 
