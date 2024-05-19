a96f79d4b02568a36108a5f678b951e33f1b44dc
Nasser Malibari <nassermalibari1@gmail.com>
2024-05-19 11:23:24 +1000

    riot path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Page1 from './pages/page1';
 import Page2 from './pages/page2';
 import ProgressPage from './pages/ProgressPage';
+import RiotText from './pages/RiotText';
 
 function App() {
 
@@ -11,8 +12,8 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
+        <Route path='/riot' element={<>1fbc4f61-eeba-4ad2-8b2b-f7efbffa9fe7</>}></Route>
         <Route path='/' element={<ProgressPage />}></Route>
-        <Route path='/riot' element={<span>1fbc4f61-eeba-4ad2-8b2b-f7efbffa9fe7</span>}></Route>
         <Route path="/page1" element={<Page1 />}></Route>
         <Route path="/page2" element={<Page2 />}></Route>
       </Routes>
